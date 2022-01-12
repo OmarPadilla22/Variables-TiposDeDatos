453a81bf1bd0b6be37654e9ab3df20b0cf55e6ff
OmarPadilla22 <omar220893@gmail.com>
2022-01-11 18:15:11 -0600

    Actividades en equipo clase datos y tipos de variables

---
+++ script.js
@@ -0,0 +1,295 @@
+//Conversión 2
+
+var x1 = false;
+
+var numero= Number(x1);
+var cadena= String(x1);
+var booleano= Boolean(x1);
+
+console.log(typeof(numero));
+console.log("El valor de " + x1 + " en number es " + numero );
+
+console.log(typeof(cadena));
+console.log("El valor de " + x1 + " en string es " + cadena);
+
+console.log(typeof(booleano));
+console.log("El valor de " + x1 + " en boolean es " + booleano);
+
+
+//Conversión 1
+
+var x2 = true;
+
+var numero= Number(x2);
+var cadena= String(x2);
+var booleano= Boolean(x2);
+
+console.log(typeof(numero));
+console.log("El valor de " + x2 + " en number es " + numero );
+
+console.log(typeof(cadena));
+console.log("El valor de " + x2 + " en string es " + cadena);
+
+console.log(typeof(booleano));
+console.log("El valor de " + x2 + " en boolean es " + booleano);
+
+
+//Conversión 3
+ 
+var x3 = 0;
+
+var numero= Number(x3);
+var cadena= String(13);
+var booleano= Boolean(x3);
+
+console.log(typeof(numero));
+console.log("El valor de " + x3 + " en number es " + numero );
+
+console.log(typeof(cadena));
+console.log("El valor de " + x3 + " en string es " + cadena);
+
+console.log(typeof(booleano));
+console.log("El valor de " + x3 + " en boolean es " + booleano);
+
+
+//Conversión 4
+
+var x4 = 1;
+
+var numero= Number(x4);
+var cadena= String(x4);
+var booleano= Boolean(x4);
+
+console.log(typeof(numero));
+console.log("El valor de " + x4 + " en number es " + numero );
+
+console.log(typeof(cadena));
+console.log("El valor de " + x4 + " en string es " + cadena);
+
+console.log(typeof(booleano));
+console.log("El valor de " + x4 + " en boolean es " + booleano);
+
+
+//Conversión 5
+
+var x5 = "0";
+
+var numero= Number(x5);
+var cadena= String(x5);
+var booleano= Boolean(x5);
+
+console.log(typeof(numero));
+console.log("El valor de " + x5 + " en number es " + numero );
+
+console.log(typeof(cadena));
+console.log("El valor de " + x5 + " en string es " + cadena);
+
+console.log(typeof(booleano));
+console.log("El valor de " + x5 + " en boolean es " + booleano);
+
+
+//Conversión 6
+
+var x6 = "000";
+
+var numero= Number(x6);
+var cadena= String(x6);
+var booleano= Boolean(x6);
+
+console.log(typeof(numero));
+console.log("El valor de " + x6 + " en number es " + numero );
+
+console.log(typeof(cadena));
+console.log("El valor de " + x6 + " en string es " + cadena);
+
+console.log(typeof(booleano));
+console.log("El valor de " + x6 + " en boolean es " + booleano);
+
+
+//Conversión 7
+
+var x7 = "1";
+
+var numero= Number(x7);
+var cadena= String(x7);
+var booleano= Boolean(x7);
+
+console.log(typeof(numero));
+console.log("El valor de " + x7 + " en number es " + numero );
+
+console.log(typeof(cadena));
+console.log("El valor de " + x7 + " en string es " + cadena);
+
+console.log(typeof(booleano));
+console.log("El valor de " + x7 + " en boolean es " + booleano);
+
+
+//Conversión 8
+
+var x8 = NaN;
+
+var numero= Number(x8);
+var cadena= String(x8);
+var booleano= Boolean(x8);
+
+console.log(typeof(numero));
+console.log("El valor de " + x8 + " en number es " + numero );
+
+console.log(typeof(cadena));
+console.log("El valor de " + x8 + " en string es " + cadena);
+
+console.log(typeof(booleano));
+console.log("El valor de " + x8 + " en boolean es " + booleano);
+
+
+//Conversión 9
+
+var x9 = Infinity;
+
+var numero= Number(x9);
+var cadena= String(x9);
+var booleano= Boolean(x9);
+
+console.log(typeof(numero));
+console.log("El valor de " + x9 + " en number es " + numero );
+
+console.log(typeof(cadena));
+console.log("El valor de " + x9 + " en string es " + cadena);
+
+console.log(typeof(booleano));
+console.log("El valor de " + x9 + " en boolean es " + booleano);
+
+
+//Conversión 10
+
+var x10 = -Infinity;
+
+var numero= Number(x10);
+var cadena= String(x10);
+var booleano= Boolean(x10);
+
+console.log(typeof(numero));
+console.log("El valor de " + x10 + " en number es " + numero );
+
+console.log(typeof(cadena));
+console.log("El valor de " + x10 + " en string es " + cadena);
+
+console.log(typeof(booleano));
+console.log("El valor de " + x10 + " en boolean es " + booleano);
+
+
+//Conversión 11
+
+var x11 = "";
+
+var numero= Number(x11);
+var cadena= String(x11);
+var booleano= Boolean(x11);
+
+console.log(typeof(numero));
+console.log("El valor de " + x11 + " en number es " + numero );
+
+console.log(typeof(cadena));
+console.log("El valor de " + x11 + " en string es " + cadena);
+
+console.log(typeof(booleano));
+console.log("El valor de " + x11 + " en boolean es " + booleano);
+
+
+//Conversión 12
+
+var x12 = "20";
+
+var numero= Number(x12);
+var cadena= String(x12);
+var booleano= Boolean(x12);
+
+console.log(typeof(numero));
+console.log("El valor de " + x12 + " en number es " + numero );
+
+console.log(typeof(cadena));
+console.log("El valor de " + x12 + " en string es " + cadena);
+
+console.log(typeof(booleano));
+console.log("El valor de " + x12 + " en boolean es " + booleano);
+
+
+//Conversión 13
+
+var x13 = "Twenty";
+
+var numero= Number(x13);
+var cadena= String(x13);
+var booleano= Boolean(x13);
+
+console.log(typeof(numero));
+console.log("El valor de " + x13 + " en number es " + numero );
+
+console.log(typeof(cadena));
+console.log("El valor de " + x13 + " en string es " + cadena);
+
+console.log(typeof(booleano));
+console.log("El valor de " + x13 + " en boolean es " + booleano);
+
+
+//Conversión 14
+
+var x14 = null;
+
+var numero= Number(x14);
+var cadena= String(x14);
+var booleano= Boolean(x14);
+
+console.log(typeof(numero));
+console.log("El valor de " + x14 + " en number es " + numero );
+
+console.log(typeof(cadena));
+console.log("El valor de " + x14 + " en string es " + cadena);
+
+console.log(typeof(booleano));
+console.log("El valor de " + x14 + " en boolean es " + booleano);
+
+
+//Conversión 15
+
+var x15 = undefined;
+
+var numero= Number(x15);
+var cadena= String(x15);
+var booleano= Boolean(x15);
+
+console.log(typeof(numero));
+console.log("El valor de " + x15 + " en number es " + numero );
+
+console.log(typeof(cadena));
+console.log("El valor de " + x15 + " en string es " + cadena);
+
+console.log(typeof(booleano));
+console.log("El valor de " + x15 + " en boolean es " + booleano);
+
+
+//Ejercicio 1: Create a Calculator
+var cumple = parseInt(prompt("Cual es tu año de nacimiento?"));
+
+var cumpleMes = parseInt(prompt("Cual es tu mes de nacimiento? (Del 1 al 12)"));
+
+var futuro = parseInt(prompt("Cual es tu año en el que quieres conocer tu edad?"));
+
+var futuroMes = parseInt(prompt("Cual es tu mes del año en el que quieres conocer tu edad?"));
+
+var resultado = futuro-cumple;
+
+if(futuroMes<=cumpleMes){
+console.log("Tu edad en el mes de " + futuroMes + " del " + futuro + " sera de " + (resultado - 1));
+}
+else{
+    console.log("Tu edad en el mes de " + futuroMes + " del " + futuro + " sera de " + resultado) ;
+}
+
+
+
+//Ejercicio 2:Create an information card
+var persona = {name : prompt("Cual es tu nombre?"), locacion : prompt("Cual es tu ubicacion?") , hobby : prompt("Cual es tu hobby?")};
+
+console.log("Hola mi nombre es " + persona.name + ", vivo en " + persona.locacion + " y disfruto " + persona.hobby +".");
+
